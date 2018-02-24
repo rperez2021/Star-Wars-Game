7e41809f10da51d080000f05efcd74d00c80cdae
rperez2021 <rob.perez.flores@gmail.com>
2018-02-23 18:29:47 -0800

    Continued work on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,50 @@
 var chewey = {
+    name: "Chewey",
     hp: 120,
     ap: 15,
     cap: 10
 }
 
 var r2d2 = {
+    name: "R2D2",
     hp: 100,
     ap: 10,
     cap: 20,
 }
 
 var luke = {
+    name: "Luke Skywalker",
     hp: 90,
     ap: 20,
     cap: 25,
 }
 
 var darthvader = {
+    name: "Darth Vader",
     hp: 125,
     ap: 15,
     cap: 10,
 }
 
 var bobafett = {
+    name: "Boba Fett",
     hp: 100,
     ap: 15,
     cap: 25
 }
 
 var kyloren = {
+    name: "Kylo Ren",
     hp: 70,
     ap: 30,
     cap: 30,
 }
 var selectedhero = "";
 var actualhero = {};
-var selectedvillan = "";
-var actualvillan = {};
+var selectedvillain = "";
+var actualvillain = {};
+var herohpbarselect = document.getElementById("herohpbar");
+var villainhpbarselect = document.getElementById("villainhpbar");
 
 //Select a hero
 function selecthero(selectedhero) {
@@ -51,36 +59,45 @@ function selecthero(selectedhero) {
             }
         }
     }
-
+    herohpbarselect.setAttribute("herohpbar", actualhero.hp);
+    herohpbarselect.innerHTML = actualhero.hp;
 }
 
-//Select a villan
-function selectvillan(selectedvillan) {
-    if (selectedvillan == "darthvader") {
-        actualvillan = darthvader;
+//Select a villain
+function selectvillain(selectedvillain) {
+    if (selectedvillain == "darthvader") {
+        actualvillain = darthvader;
     } else {
-        if (selectedvillan == "bobafett") {
-            actualvillan = bobafett;
+        if (selectedvillain == "bobafett") {
+            actualvillain = bobafett;
         } else {
-            if (selectedvillan == "kyloren") {
-                actualvillan = kyloren
+            if (selectedvillain == "kyloren") {
+                actualvillain = kyloren
             }
         }
     }
-
+    villainhpbarselect.setAttribute("villainhpbar", actualvillain.hp);
+    villainhpbarselect.innerHTML = actualvillain.hp;
 }
 
 //Start the match 
 
 //Attack Function
-function attack(actualhero, actualvillan) {
-    actualvillan.hp = actualvillan.hp - actualhero.ap;
+function attack(actualhero, actualvillain) {
+    actualvillain.hp = actualvillain.hp - actualhero.ap;
     var journaladd = document.getElementById("lightjournal");
-    var 
-    journaladd.appendChild(document.createElement("p"));
+    var createline = document.createElement("p");
+    createline.id = "createline";
+    createline.innerHTML = actualhero.name + " attacked " + actualvillain.name + " for " +
+        actualhero.ap + " damage!";
+    journaladd.appendChild(createline);
+    document.getElementById("villainhpbar").setAttribute(villainhpbar, actualvillain.hp)
 }
 
 //CounterAttack Function
+function counterattack(actualhero, actualvillain) {
+    
+}
 
 //HP Consolidation Function
 
